088833bf6887c5c20dfaa1a9761daea5216ff4f5
Adnan Ahmed <adnan.ahmed01@gmail.com>
2020-05-13 23:31:47 +0100

    added select restaurant functionality

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,32 +4,18 @@ const { list } = require('../modules/restaurants')
 
 module.exports = async function (request, response) {
 
-  const restaurants = await list();
-  const cities = await getCities(
-    restaurants.map(element => element.location )
+const { city } = request.query;
+
+const [ restaurants, allRestaurants ] = await Promise.all( [list(city), list()] )
+
+const cities = await getCities(
+  allRestaurants.map(element => element.location )
   );
+
  
-  const { city } = request.query;
-
-  const printRequest = function(chosenCity, restaurants) {
-
-  const chosenCapitalized = chosenCity.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
-
-  const arrayRestaurant = [];
-  
-  restaurants.forEach(element => {
-   if (chosenCapitalized === element.location) {
-     arrayRestaurant.push(element.name);
-   }
-  })
-
-  return arrayRestaurant;
-
-  }
 
 
-
-  return response.render('home.html', { selectedCity: city, restaurants, cities, printRequest }  );
+  return response.render('home.html', { selectedCity: city, restaurants: restaurants, cities }  );
 };
  
 
